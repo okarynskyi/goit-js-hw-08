9bc7a83b659d56aba68631253b9a3172214c2dd7
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-29 18:25:43 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const getTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.on('timeupdate', throttle(playerOn, 1000));
+
+player.setCurrentTime(getTime);
+
+function playerOn(data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
