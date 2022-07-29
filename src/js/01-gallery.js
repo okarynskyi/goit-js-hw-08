be88b47ba82a12d0e2daa95ef40a5c9b851a9fa1
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-29 13:33:07 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const markup = createLi(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+// gallery.addEventListener('click', listHandler);
+
+function createLi(array) {
+    return array.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>` ," ")
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250'
+});
