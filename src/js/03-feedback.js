71f873b7194bae67330a0524f5db6f2e9fdbc58f
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-29 23:14:40 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector("form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const formData = {};
+const savedMassege = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+form.addEventListener("input", throttle(saveInput, 500));
+form.addEventListener("submit", submitInput);
+
+populateInput()
+
+function saveInput(evt) {
+    evt.preventDefault();
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function submitInput(evt) {
+    evt.preventDefault();
+    console.log(savedMassege)
+
+    evt.currentTarget.reset()
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+}
+
+function populateInput() {
+    // const savedMassege = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (savedMassege) {
+        form.elements.email.value = savedMassege.email;
+        form.elements.message.value = savedMassege.message
+    }
+}
