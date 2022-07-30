1591a5c233ae2de9aa789e27a3ba255efce9e2d7
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-07-30 18:18:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,8 +2,6 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector("form");
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const formData = {};
-const savedMassege = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
 form.addEventListener("input", throttle(saveInput, 500));
 form.addEventListener("submit", submitInput);
@@ -12,20 +10,31 @@ populateInput()
 
 function saveInput(evt) {
     evt.preventDefault();
+    let formData = localStorage.getItem(LOCALSTORAGE_KEY)
+    ? JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+    : {};
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-}
+    }
 
 function submitInput(evt) {
     evt.preventDefault();
-    console.log(savedMassege)
+    const email = evt.target.elements.email.value;
+    const message = evt.target.elements.message.value;    
+    if (email === '' || message === '') {
+        alert('Please fill in all the fields!');
+    } else {
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+        console.log({ email, message });
+    }
     evt.currentTarget.reset()
-    localStorage.removeItem(LOCALSTORAGE_KEY)
 }
 
 function populateInput() {
-    if (savedMassege) {
-        form.elements.email.value = savedMassege.email;
-        form.elements.message.value = savedMassege.message
+    let savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (savedData) {
+        savedData = JSON.parse(savedData);
+        form.elements.email.value = savedData.email;
+        form.elements.message.value = savedData.message
     }
 }
